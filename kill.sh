a9139cc089726cc7f2b38bc09cdefeb05697e72d
Cameron Griffin (He/Him) <101752687+CodrCam@users.noreply.github.com>
2022-11-14 10:45:07 -0800

    Update kill.sh

--- kill.sh
+++ kill.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-
+# displays the program PIDs
 ps -e
-
+# Prints a Quetion for input
 echo "Hello enter the PID of program you want to kill?"
-
+# Creates var to input in to kill for loop
 read KillPID
-
+# is a for loop to kill off a single PID entered by user
 for i in KillPID
 do
 kill $KillPID
